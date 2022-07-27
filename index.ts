abdc08cc65e440a8355b8cfc18c0abbeb9238592
IvoPaunov <ivo.paunov@gmail.com>
2022-07-27 15:26:39 +0300

    add index

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { AmbireDappManifest } from './src/types'
 import { getWalletGnosisDefaultList, getGnosisDefaultList } from './src/utils'
 
-export function getWalletDappCatalog(): AmbireDappManifest[] {
+function getWalletDappCatalog(): AmbireDappManifest[] {
     const dappCatalog = getWalletGnosisDefaultList()
         .concat(getGnosisDefaultList())
 
     return dappCatalog
-} 
+}
+
+export * from './src/types'
+export * from './src/utils'
+export * from './src/networks'
+
+export default  getWalletDappCatalog
