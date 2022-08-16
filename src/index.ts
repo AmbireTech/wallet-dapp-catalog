10a7a0c53d8f9ced8fe727c49407f58e617e1cdc
IvoPaunov <ivo.paunov@gmail.com>
2022-08-16 16:59:42 +0300

    update ts cfg - still not working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import gnosisDefaultList from './catalogs/gnosis-default.applist.json'
-import walletGnosisDefaultCatalog from './catalogs/ambire-wallet-gnosis-default.applist.json'
-import walletWalletconnectDefaultCatalog from './catalogs/ambire-wallet-walletconnect-default.applist.json'
+const gnosisDefaultList = require('./catalogs/gnosis-default.applist.json')
+const walletGnosisDefaultCatalog = require('./catalogs/ambire-wallet-gnosis-default.applist.json')
+const walletWalletconnectDefaultCatalog = require('./catalogs/ambire-wallet-walletconnect-default.applist.json')
 import { NetworkId } from 'ambire-common/src/constants/networks'
-import { AmbireDappManifest, WalletConnectionType, chainIdToWalletNetworkId } from 'ambire-common/src/services/dappCatalog'
+import { AmbireDappManifest, WalletConnectionType } from 'ambire-common/src/services/dappCatalog/types'
+import { chainIdToWalletNetworkId } from 'ambire-common/src/services/dappCatalog/dappCatalogUtils'
+const fs = require('fs')
+const path = require('path')
+const mkdirp = require('mkdirp')
 
 function getGnosisDefaultList(): Array<AmbireDappManifest> {
     const asWalletDapps = gnosisDefaultList.apps.map((dapp: any) => {
@@ -19,7 +23,7 @@ function getGnosisDefaultList(): Array<AmbireDappManifest> {
 }
 
 function getWalletGnosisDefaultList(): Array<AmbireDappManifest> {
-    const walletGnosisDapps: AmbireDappManifest[] = walletGnosisDefaultCatalog.apps
+    const walletGnosisDapps: Array<AmbireDappManifest> = walletGnosisDefaultCatalog.apps
         .map((d: any) => ({
             ...d,
             connectionType: WalletConnectionType.gnosis,
@@ -30,7 +34,7 @@ function getWalletGnosisDefaultList(): Array<AmbireDappManifest> {
 }
 
 export function getWalletWalletconnectDefaultList(): Array<AmbireDappManifest> {
-    const walletGnosisDapps: AmbireDappManifest[] = walletWalletconnectDefaultCatalog.apps
+    const walletGnosisDapps: Array<AmbireDappManifest> = walletWalletconnectDefaultCatalog.apps
         .map((d: any) => ({
             ...d,
             connectionType: WalletConnectionType.walletconnect,
@@ -48,6 +52,21 @@ function getWalletDappCatalog(): Array<AmbireDappManifest> {
     return dappCatalog
 }
 
-module.exports = {
-    getWalletDappCatalog
+const catalogs = JSON.stringify(getWalletDappCatalog())
+
+const fileDir = path.join(__dirname, 'build')
+const filePath = path.join(fileDir, 'ambire-wallet-dapp-catalog.json')
+
+const writeFile = async () => {
+    try {
+        await mkdirp(fileDir)
+        await fs.writeFileSync(filePath, catalogs)
+        console.log('Catalog exported!')
+        process.exit(0)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
 }
+
+writeFile()
